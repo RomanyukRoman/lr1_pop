3cefc3b67afe048d4f410c3c88127e07db55530c
RomanyukRoman <romaniuk.roman1121@vu.cdu.edu.ua>
2024-03-20 14:22:02 +0200

    task_id

--- ada/src/main.adb
+++ ada/src/main.adb
@@ -1,28 +1,33 @@
 with Ada.Text_IO; use Ada.Text_IO;
---with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Main is
 
    can_stop : boolean := false;
    pragma Volatile(can_stop);
-   count: Integer := 8;
+   count: Integer := 5;
    terms: Long_Integer := 0;
    step: Integer := 2;
    --type Delay_Array is array(1..count) of Integer;
    --Delays : Delay_Array := (1, 5, 10);
 
    task type Stop_Task;
-   task type Sum_Task(task_id : Integer);
+   task type Sum_Task is
+      entry id_sum_task(task_id: in Integer);
+   end Sum_Task;
 
    task body Stop_Task is
    begin
-      delay 2.0;
+      delay 1.0;
       can_stop := true;
    end Stop_Task;
 
    task body Sum_Task is
       sum : Long_Long_Integer := 0;
+      task_id : Integer;
    begin
+      accept id_sum_task (task_id : in Integer) do
+          Sum_Task.task_id := task_id;
+      end id_sum_task;
       loop
          exit when can_stop;
          sum := sum + Long_Long_Integer(step);
@@ -33,11 +38,10 @@ procedure Main is
    end Sum_Task;
 
    stop: Stop_Task;
-   threads: array(1..count) of access Sum_Task;
+   threads: array(1..count) of Sum_Task;
 
 begin
    for i in 1..count loop
-      threads(i) := new Sum_Task(i);
+      threads(i).id_sum_task(i);
    end loop;
-   null;
 end Main;
